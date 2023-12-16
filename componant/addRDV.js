ebe0c475408fcb6086055d7d6c7d11a50cc762a8
Rami <rami.zbidi@horizon-tech.tn>
2023-12-16 13:18:51 +0100

    Add input controls over the date and hour fields

--- componant/addRDV.js
+++ componant/addRDV.js
@@ -6,11 +6,15 @@ import { styles } from '../styles/styles';
 // On this page the user will enter the data about the appointment
 export default function AddRDV ()
 {
-
+    const validHourRegex = /^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/;
+    const validDateRegex = /^(31\/(01|03|05|07|08|10|12)\/\d{4}|(0[1-9]|1[0-9]|2[0-8])\/(0[1-9]|1[0-2])\/\d{4}|29\/02\/\d{4}([02468][048]|[13579][26])|(0[1-9]|1[0-9]|2[0-9])\/(0[13-9]|1[0-2])\/\d{4}|30\/(01|03|04|05|06|07|08|09|10|11|12)\/\d{4})$/;
+    ;
     const [date, setDate] = useState('');
+    const [dateInvalid, setDateInvalid] = useState(false);
     const [title, setTitle] = useState('');
     const [content, setContent] = useState('');
     const [hour, setHour] = useState('');
+    const [hourInvalid, setHourInvalid] = useState(false);
 
     const resetFields = () =>
     {
@@ -18,6 +22,30 @@ export default function AddRDV ()
         setTitle('');
         setContent('');
         setHour('');
+        setHourInvalid(false);
+        setDateInvalid(false);
+    }
+
+    const addItem = () =>
+    {
+        if (!date.match(validDateRegex))
+        {
+            setDateInvalid(true);
+            return
+        }
+
+        if (!hour.match(validHourRegex))
+        {
+            setHourInvalid(true);
+            return
+        }
+
+        setDate('');
+        setTitle('');
+        setContent('');
+        setHour('');
+        setHourInvalid(false);
+        setDateInvalid(false);
     }
 
     return (
@@ -25,53 +53,65 @@ export default function AddRDV ()
             <View style={styles.container}>
                 <Text style={styles.header}>Add an appointment</Text>
 
+                {/*The error messages view*/}
+                <View>
+                    {hourInvalid && <Text style={styles.errorMsg} >Correct the entered hour</Text>}
+                    {dateInvalid && <Text style={styles.errorMsg} >Correct the entered date</Text>}
+                </View>
+
+                {/*The date input field*/}
                 <View>
                     <Text style={styles.label}>Date</Text>
                     <View style={styles.inputArea}>
                         <Icon name="event" size={20} color="#7e7e7e" style={{ marginRight: 10 }} />
                         <TextInput
-                        placeholder="Enter the date of the appointment"
+                        placeholder="Enter the date as dd/mm/yyyy"
                         onChangeText={(text) => setDate(text)}
                         value={date}/>
                     </View>
                 </View>
 
+                {/*The title input field*/}
                 <View>
                     <Text style={styles.label}>Title</Text>
                     <View style={styles.inputArea}>
                         <Icon name="text-fields" size={20} color="#7e7e7e" style={{ marginRight: 10 }} />
                         <TextInput
-                        placeholder="Enter the title of the appointment"
+                        placeholder="Enter the title"
                         onChangeText={(text) => setTitle(text)}
                         value={title}/>
                     </View>
                 </View>
 
+                {/*The content input field*/}
                 <View>
                     <Text style={styles.label}>Content</Text>
                     <View style={styles.inputArea}>
                         <Icon name="format-align-left" size={20} color="#7e7e7e" style={{ marginRight: 10 }} />
                         <TextInput
-                        placeholder="Enter the content of the appointment"
+                        placeholder="Enter the content"
                         onChangeText={(text) => setContent(text)}
                         value={content}/>
                     </View>
                 </View>
 
+                {/*The hour input field*/}
                 <View>
                     <Text style={styles.label}>Hour</Text>
                     <View style={styles.inputArea}>
                         <Icon name="access-time" size={20} color="#7e7e7e" style={{ marginRight: 10 }} />
                         <TextInput
-                        placeholder="Enter the hour of the appointment"
+                        placeholder="Enter the hour as 00:00"
                         onChangeText={(text) => setHour(text)}
                         value={hour}/>
                     </View>
                 </View>
 
+                {/*The buttons view*/}
                 <View style={styles.button}>
                     <Button
                     color='#9955bb'
+                    onPress={addItem}
                     title="Add"/>
 
                     <Button
